ad8c67e9186db83126487c9493d274035d034f85
Patriciaahp <patrihernandezperez9@gmail.com>
2025-03-01 19:04:53 +0100

    revisar

--- get_next_line.c
+++ get_next_line.c
@@ -17,96 +17,110 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
+int ft_strlen(const char *str)
 {
-	size_t	i;
-	size_t	j;
-
-	j = 0;
-	i = 0;
-	while (src[j] != '\0')
-		j++;
-	if (size == 0)
-		return (j);
-	while ((src[i] != '\0') && (size != 1))
-	{
-		dst[i] = src[i];
-		i++;
-		size--;
-	}
-	if (size > 0)
-		dst[i] = '\0';
-	return (j);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*s3;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	s3 = malloc(sizeof(char) * (i + j + 1));
-	if (!s3)
-		return (NULL);
-	ft_strlcpy(s3, s1, i + 1);
-	ft_strlcpy(s3 + i, s2, j + 1);
-	return (s3);
-}
-
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (*str)
-		i++;
-	return (i);
+    int i = 0;
+    while (str[i])
+        i++;
+    return i;
 }
 
 char *ft_strchr(const char *s, int c)
 {
-	unsigned int	i;
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] == (char)c)
+            return (char *)(s + i);
+        i++;
+    }
+    if (c == '\0' && s[i] == '\0')
+        return (char *)(s + i);
+    return NULL;
+}
 
-	i = 0;
-	if ((char)c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
-	while (*s)
-	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-		i++;
-	}
-	return (NULL);
+char *ft_strjoin(const char *s1, const char *s2)
+{
+    int len1 = ft_strlen(s1);
+    int len2 = ft_strlen(s2);
+    char *result = malloc(len1 + len2 + 1);
+    int i = 0;
+
+    if (!result)
+        return NULL;
+
+    while (i < len1)
+    {
+        result[i] = s1[i];
+        i++;
+    }
+    while (i < len1 + len2)
+    {
+        result[i] = s2[i - len1];
+        i++;
+    }
+    result[i] = '\0';
+    return result;
+}
+
+char *extract_line(char **resto)
+{
+    int i = 0;
+    while ((*resto)[i] && (*resto)[i] != '\n')
+        i++;
+
+    char *line = malloc(i + 2);
+    if (!line)
+        return NULL;
+
+    int j = 0;
+    while (j <= i)
+    {
+        line[j] = (*resto)[j];
+        j++;
+    }
+    line[j] = '\0';
+
+    char *new_resto = NULL;
+    if ((*resto)[i] == '\n')
+        i++;
+
+    if ((*resto)[i] != '\0')
+        new_resto = ft_strjoin(*resto + i, "");
+    free(*resto);
+    *resto = new_resto;
+
+    return line;
 }
 
 char *get_next_line(int fd)
 {
-	//cambiar buffer por BUFFER_SIZE
-	int	i;
-
-	i = 0;
-	if (fd < 0)
-	return NULL;
-    char *buffer = malloc(20);
-
-    if (!buffer)
+    static char *resto = NULL;
+    char *buffer = malloc(BUFFER_SIZE + 1);
+    if (fd < 0 || BUFFER_SIZE <= 0 || !buffer)
         return NULL;
 
-    ssize_t bytes_leidos = read(fd, buffer, 20);
-    if (bytes_leidos <= 0)
+    int bytes_leidos = 1;
+    while (!ft_strchr(resto, '\n') && bytes_leidos > 0)
     {
-        free(buffer);
-        return NULL;
+        bytes_leidos = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_leidos <= 0)
+        {
+            free(buffer);
+            if (resto && *resto)
+                return extract_line(&resto);
+            return NULL;
+        }
+        buffer[bytes_leidos] = '\0';
+        resto = ft_strjoin(resto, buffer);
     }
 
-    buffer[17] = '\0';
-    return buffer;
+    free(buffer);
+    return extract_line(&resto);
 }
 
 int main()
@@ -118,15 +132,11 @@ int main()
         return 1;
     }
 
-    char *letra = get_next_line(fd);
-    if (letra)
+    char *line;
+    while ((line = get_next_line(fd)))
     {
-        printf("Letra leída: %s\n", letra);
-        free(letra);
-    }
-    else
-    {
-        printf("No se pudo leer una letra.\n");
+        printf("%s", line);
+        free(line);
     }
 
     close(fd);
